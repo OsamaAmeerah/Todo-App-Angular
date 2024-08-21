75d8a993b70e699dad996e0fc19e7a7a65a3bafb
osama ameerah <osamaameera32@gmail.com>
2024-08-21 23:01:14 +0300

    Finish the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,20 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
-import { TasksComponent } from "./tasks/tasks.component";
 import { BrowserModule } from "@angular/platform-browser";
-import { CardComponent } from "./shared/card/card.component";
-import { TaskComponent } from "./tasks/task/task.component";
-import { NewTaskComponent } from "./tasks/new-task/new-task.component";
-import { DatePipe } from "@angular/common";
-import { FormsModule } from "@angular/forms";
+import { SharedModule } from "./shared/shared.module";
+import { TasksModule } from "./tasks/tasks.module";
 
 @NgModule({
   declarations : [
-    AppComponent,HeaderComponent,UserComponent,CardComponent,
- TasksComponent,TaskComponent,NewTaskComponent,
+ AppComponent,
+ HeaderComponent,
+ UserComponent,
   ],
   imports:[
     BrowserModule,
-    FormsModule,
+    SharedModule,
+    TasksModule
   ],
   bootstrap: [
     AppComponent
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,14 +1,10 @@
 import { Component, Input, } from '@angular/core';
-import { TaskComponent } from './task/task.component';
-import { NewTaskComponent } from './new-task/new-task.component';
-import { NewTaskData } from './task/task.model';
 import { TasksService } from './tasks.service';
 
 
 
 @Component({
   selector: 'app-tasks',
-
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
